5e205fadb2c3cf5467470993163dd20f07c5773e
KBumstead <115068127+KBumstead@users.noreply.github.com>
2023-10-10 13:17:46 +0700

    added button function for the header in home login and register page

---
+++ HomePage.js
@@ -0,0 +1,10 @@
+
+document.getElementById('login-header').addEventListener('click', function() {
+    // Navigate to another HTML page
+    window.location.href = 'LoginPage.html';
+});
+
+document.getElementById('register-header').addEventListener('click', function() {
+    // Navigate to another HTML page
+    window.location.href = 'RegisterPage.html';
+});
